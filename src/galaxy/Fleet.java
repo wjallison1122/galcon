81bd71947df3e5e203eafdb907997456144a9e01
alelazar <alexandazar@gmail.com>
2017-10-19 12:53:31 -0700

    Added documentation to Fleet (#50)

--- src/galaxy/Fleet.java
+++ src/galaxy/Fleet.java
@@ -5,6 +5,12 @@ public class Fleet extends Unit {
     public final Coords VELOCITY;
     private int ticsLeft;
 
+    /**
+     * Creates a new Fleet. Should only be called from {link {@link Planet#sendFleet(Planet, int)}
+     * @param units The number of units in the fleet.
+     * @param start The Planet this Fleet starts from.
+     * @param destination The Planet this Fleet is targeting.
+     */
     Fleet(int units, Planet start, Planet destination) {
         super(start.getOwner(), units, start);
         DESTINATION = destination;
@@ -13,18 +19,32 @@ public class Fleet extends Unit {
         ticsLeft = (int)(distanceLeft / GameSettings.FLEET_SPEED) + 1;
     }
 
+    /**
+     * Determines whether this Fleet is targeting the given Planet
+     * @param p The Planet in question
+     * @return Whether this Fleet is targeting that Planet
+     */
     public boolean targeting(Planet p) {
         return DESTINATION == p;
     }
 
+    /**
+     * @return The number of game tics until the Fleet hits its target.
+     */
     public int ticsLeft() {
         return ticsLeft;
     }
 
+    /**
+     * @return Whether the Fleet has hit its target yet.
+     */
     public boolean hasHit() {
         return ticsLeft == 0;
     }
 
+    /**
+     * Moves the Fleet. Hits the target Planet if the Fleet has finished its travel.
+     */
     @Override
     void update() {
         if (hasHit()) {
@@ -40,8 +60,12 @@ public class Fleet extends Unit {
         }
     }
 
+    /**
+     * Will eventually have all the info needed to write games to file.
+     */
     @Override
     public String toString() {
+        // TODO Make this meaningful
         return super.toString();
     }
 }
